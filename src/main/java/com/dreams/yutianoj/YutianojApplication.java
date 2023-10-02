4d577863fe9cb734794f1f099aee7c66a77312d6
PoemsAndDreams <3513254476@qq.com>
2023-10-02 10:18:20 +0800

    add:项目初始化

---
+++ src/main/java/com/dreams/yutianoj/YutianojApplication.java
@@ -0,0 +1,13 @@
+package com.dreams.yutianoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YutianojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YutianojApplication.class, args);
+    }
+
+}
